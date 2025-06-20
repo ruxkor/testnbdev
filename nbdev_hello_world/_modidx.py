b3e39b60e56be456a6780a2d674a5dca3a9304ac
Andor Goetzendorff <andor.goetzendorff@kraken.tech>
2025-06-20 16:39:40 +0200

    more2

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://fastai.github.io',
                 'git_url': 'https://github.com/fastai/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': { 'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py'),
+                                        'nbdev_hello_world.core.foo2': ('core.html#foo2', 'nbdev_hello_world/core.py')}}}
